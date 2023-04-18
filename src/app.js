dddc5aabc73cec0e70b9e1fd7eb471ac8ede239e
Gabriela Oliveira <gabrielamontanhini@gmail.com>
2023-04-18 02:48:28 -0300

    Implementação de rotas

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express"
 import cors from "cors"
-import { MongoClient } from "mongodb"
+import { MongoClient, ObjectId } from "mongodb"
 import dotenv from "dotenv"
 
 //Criação do servidor
@@ -13,7 +13,7 @@ dotenv.config()
 
 //Conexão com o banco de dados
 const mongoClient = new MongoClient(process.env.DATABASE_URL)
-try{
+try {
     await mongoClient.connect()
     console.log("MongoDB conectado!")
 } catch (err) {
@@ -22,8 +22,84 @@ try{
 const db = mongoClient.db()
 
 
+//Endpoints
 
+app.post("/cadastro", async (req, res) => {
+    const novoUsuario = req.body
+    try {
+        await db.collection("usuarios").insertOne(novoUsuario)
+        return res.sendStatus(201)
+    }
+    catch (err) {
+        res.status(500).send(err.message)
+    }
+})
 
+app.get("/cadastro", async (req, res) => { //VER OS USUARIOS
+    try {
+        const usuarios = await db.collection("usuarios").find().toArray()
+        return res.status(200).send(usuarios)
+    }
+    catch (err) {
+        res.status(500).send(err.message)
+    }
+})
+
+app.post("/login", async (req, res) => {
+    const { email, senha } = req.body
+    try {
+        const login = await db.collection("usuarios").findOne({ email, senha })
+        if (!login) {
+            return res.status(404).send("Incorretos")
+        }
+        return res.sendStatus(200)
+    }
+    catch (err) {
+        res.status(500).send(err.message)
+    }
+})
+
+app.post("/nova-transacao/:tipo", async (req, res) => {
+const {tipo} = req.params
+const {valor} = req.body
+
+const novoMovimento = {
+    valor: valor,  
+    tipo: tipo
+}
+
+try{
+    const operação = await db.collection("movimento").insertOne(novoMovimento)
+    return res.status(201).send("Movimento adicionado!")
+}
+catch (err) {
+    res.status(500).send(err.message)
+}
+})
+
+app.get("/home", async (req, res) => { //precisará de token
+try{
+    const extrato = await db.collection("movimento").find().toArray()
+    res.status(200).send(extrato)
+}
+catch (err) {
+    res.status(500).send(err.message)
+}
+})
+
+app.delete("/home/:id", async (req,res) => {
+    const { id } = req.params
+
+try{
+    const deletar = await db.collection("movimento").deleteOne({ _id: new ObjectId(id) })
+    if (deletar.deletedCount === 0) return res.status(404).send("Esse item não existe!")
+    res.send("Item deletado com sucesso!")
+}
+
+catch (err) {
+    res.status(500).send(err.message)
+}
+})
 
 // Deixa o app escutando, à espera de requisições
 const PORT = 5000
